d3a7a1a91f8d6357d53884692156fe62c46f42a5
phanivelli <vdurgaphanikumar@gmail.com>
2018-10-29 19:35:36 +0530

    even or odd

---
+++ EvenOrOdd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int num;
+    printf("enter the number to check whether the number is even or odd=");
+    scanf(" %d ", &num);
+
+    // conditional operator
+    (num%2 ==0 ) ? printf("%d your number is even",num) : printf("%d your number is odd",num);
+    return 0;
+}
