5d8fa72c98b6928eb89a7361fa39944748df5b95
phanivelli <vdurgaphanikumar@gmail.com>
2020-10-11 13:50:20 +0530

    C programms

---
+++ Demo.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int var = 20;
+    printf("%d",var);
+{
+    int var = 30;
+    printf("%d",var);
+}
+}
--- EvenOrOdd.c
+++ EvenOrOdd.c
@@ -4,7 +4,6 @@ int main(){
     int num;
     printf("enter the number to check whether the number is even or odd=");
     scanf(" %d ", &num);
-
-    (num%2 ==0 ) ? printf("%d your number is even",num) : printf("%d your number is odd",num);
+    num%2 ==0  ? printf("%d your number is even",num) : printf("%d your number is odd",num);
     return 0;
 }
